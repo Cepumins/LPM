db985d2263c14322ee0e07b3f20765c9d33846fc
Cepumins <cepumm@gmail.com>
2024-07-28 19:32:59 +0300

    0.1 LPM

---
+++ src/features/stocks/ConfirmOrderPopup.js
@@ -0,0 +1,151 @@
+import React, { forwardRef, useEffect } from 'react';
+import './ConfirmOrderPopup.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLeftLong } from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios';
+
+const taxAmount = 0.01;
+
+const ConfirmOrderPopup = forwardRef(({ orderDetails, onClose, createOrderPopupRef, orderExecution }, ref) => {
+  const { ticker, orderType, quantity, price } = orderDetails;
+
+  /*
+  //console.log(`Ticker: ${orderDetails.ticker}`);
+  //console.log(`Order Type: ${orderType}`);
+  //console.log(`Quantity: ${quantity}`);
+  //console.log(`Price: ${price}`);
+
+  useEffect(() => {
+    console.log("ConfirmOrderPopup received orderDetails:", orderDetails);
+  }, [orderDetails]);
+  */
+
+  useEffect(() => {
+    const handleOverlayClick = (e) => {
+      if (ref.current && !ref.current.contains(e.target)) {
+        onClose();
+      }
+    };
+
+    document.addEventListener('mousedown', handleOverlayClick);
+
+    return () => {
+      document.removeEventListener('mousedown', handleOverlayClick);
+    };
+  }, [onClose, ref]);
+
+  
+
+  const total = (parseFloat(price) * parseInt(quantity)).toFixed(2);
+  const received = (total - total * taxAmount).toFixed(2);
+
+  const handleConfirm = async () => {
+    //console.log("Entering handleConfirm function");
+    //alert(`1`);
+    try {
+      //alert(`creating order..`);
+      await axios.post(`http://localhost:5000/api/stocks/data/order`, {
+        ticker,
+        orderType,
+        quantity,
+        price: parseFloat(price),
+        orderExecution
+      });
+      alert(`Created ${orderExecution} ${orderType} order for ${quantity} shares of ${ticker} at $${price} each.`);      
+
+      /*
+      if (orderExecution === 'market') {
+        await axios.post('http://localhost:5000/api/stocks/fulfill-order', {
+          type: 'market',
+          ticker,
+          action: orderType,
+          quantity,
+          price: parseFloat(price)
+        });
+        alert(`${ticker} stock ${orderType}ing at $${price} (Q: ${quantity}) was successful.`);      
+      } else if (orderExecution === 'book') {
+        await axios.post(`http://localhost:5000/api/stocks/data/order`, {
+          ticker,
+          orderType,
+          quantity,
+          price: parseFloat(price),
+          orderExecution
+        });
+        alert(`Created ${orderType} order for ${quantity} shares of ${ticker} at $${price} each.`);        
+      }*/
+      
+      //onConfirm();
+      onClose();
+    } catch (error) {
+      console.error(`Error creating ${orderType} order:`, error);
+      alert(`Failed to create order. Please try again: ${error}`);
+    }
+  };
+
+  useEffect(() => {
+    // Function to handle keydown event
+    const handleKeyDown = (event) => {
+      if (event.key === 'Enter') {
+        handleConfirm();
+      }
+    };
+
+    // Add event listener for keydown
+    window.addEventListener('keydown', handleKeyDown);
+
+    // Clean up event listener on component unmount
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [handleConfirm]);
+
+  /*
+  useEffect(() => {
+    const handleOverlayClick = (e) => {
+      if (
+        ref.current && 
+        !ref.current.contains(e.target) && 
+        (!createOrderPopupRef || (createOrderPopupRef.current && !createOrderPopupRef.current.contains(e.target)))
+      ) {
+        onClose();
+      }
+    };
+
+    document.addEventListener('mousedown', handleOverlayClick);
+
+    return () => {
+      document.removeEventListener('mousedown', handleOverlayClick);
+    };
+  }, [onClose, ref, createOrderPopupRef]);
+  */
+
+  return (
+    <div className="confirm-order-popup">
+      <div className="order-content" ref={ref}>
+        <button className="close-arrow" onClick={onClose}>
+          <FontAwesomeIcon icon={faLeftLong} />
+        </button>
+
+        <h2>Confirm {orderType.charAt(0).toUpperCase() + orderType.slice(1)} Order</h2>
+        <img src={`/logos/${ticker.toLowerCase()}.svg`} alt={`${ticker} icon`} className="order-stock-icon" />
+        <p className='font-bold'>{ticker}</p>
+
+        <div className="order-details">
+          <p>Price: ${price}</p>
+          <p>Quantity: {quantity}</p>
+          <p>Total: ${total}</p>
+          {orderType === 'sell' && <p>Received (after tax): ${received}</p>}
+        </div>
+
+        <button 
+          className="confirm-button" 
+          onClick={handleConfirm}
+        >
+          Confirm
+        </button>
+      </div>
+    </div>
+  );
+});
+
+export default ConfirmOrderPopup;
---
+++ src/features/stocks/StockList.js
@@ -0,0 +1,206 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './StockList.css';
+import StockPopup from './StockPopup';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
+
+function StockList({ userId, userBalance, inventory, refreshUserData }) {
+  const [stocks, setStocks] = useState([]);
+  const [selectedStock, setSelectedStock] = useState(null);
+  const [sortOption, setSortOption] = useState('default');
+  const [isDropdownDisabled, setDropdownDisabled] = useState(false);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  useEffect(() => {
+    const fetchStocks = async () => {
+      try {
+        const response = await axios.get('http://localhost:5000/api/stocks/info');
+        const fetchedStocks = response.data;
+        setStocks(fetchedStocks);
+        sortStocks(sortOption, fetchedStocks);
+        //sortStocks('default', fetchedStocks); // Sort alphabetically initially
+      } catch (error) {
+        console.error('Error fetching stocks:', error);
+      }
+    };
+
+    fetchStocks();
+
+    const ws = new WebSocket('ws://localhost:5000');
+    ws.onmessage = (event) => {
+      const message = JSON.parse(event.data);
+      if (message.type === 'update') {
+        if (Array.isArray(message.data)) {
+          setStocks(prevStocks => {
+            const updatedStocks = [...prevStocks];
+            message.data.forEach(updatedStock => {
+              const index = updatedStocks.findIndex(stock => stock.ticker === updatedStock.ticker);
+              if (index !== -1) {
+                console.log(`ARRAY: ${updatedStock.ticker} new buy: ${updatedStock.buyP} and sell: ${updatedStock.sellP}`);
+                updatedStocks[index] = { ...updatedStocks[index], ...updatedStock };
+              }
+            });
+            return updatedStocks;
+          });
+        } else if (typeof message.data === 'object' && message.data.ticker) {
+          setStocks(prevStocks => {
+            const updatedStocks = [...prevStocks];
+            const index = updatedStocks.findIndex(stock => stock.ticker === message.data.ticker);
+            if (index !== -1) {
+              console.log(`OBJECT: ${message.data.ticker} new sell: ${message.data.sellP} and buy: ${message.data.buyP}`);
+
+              updatedStocks[index] = { ...updatedStocks[index], ...message.data };
+              if (selectedStock && updatedStocks[index].ticker === selectedStock.ticker) {
+                setSelectedStock(updatedStocks[index]);
+              
+              }
+              //console.log('selected stock:');
+              //console.log(selectedStock);
+            }
+            //sortStocks(sortOption, updatedStocks);
+            return updatedStocks;
+          });
+        }
+      }
+    };
+
+    return () => {
+      ws.close();
+    };
+  }, [selectedStock, sortOption]);
+
+  /*
+  useEffect(() => {
+    console.log(`Selected stock:`, selectedStock);
+  }, [selectedStock]);
+  */
+
+  const handleStockClick = (stock) => {
+    //console.log(`Stock clicked: ${stock.ticker}`);
+    setSelectedStock(stock);
+  };
+
+  const closePopup = () => {
+    console.log('Popup closed');
+    setSelectedStock(null);
+  };
+
+  const refreshStock = (ticker) => {
+    const updatedStock = stocks.find(s => s.ticker === ticker);
+    setSelectedStock(updatedStock);
+  };
+
+  const handleSortChange = (e) => {
+    const newSortOption = e.target.value;
+    if (newSortOption !== sortOption) {
+      console.log(`applying sorting as old was ${sortOption} and the new is ${newSortOption}`);
+      //sortOption = newSortOption;
+      setSortOption(newSortOption);
+      sortStocks(newSortOption, stocks);
+    } else {
+      console.log(`new and old sorting options both are ${sortOption}`);
+    }
+    setDropdownDisabled(true);
+    setTimeout(() => {
+      setDropdownDisabled(false);
+    }, 750);
+  };
+
+  const sortStocks = (option, fetchedStocks = stocks) => {
+    console.log(`sorting to ${option}`);
+    let sortedStocks = [...fetchedStocks];
+  
+    const sortFunction = (a, b, key, ascending = true) => {
+      const aValue = isNaN(parseFloat(a[key])) ? (ascending ? Number.POSITIVE_INFINITY : Number.NEGATIVE_INFINITY) : parseFloat(a[key]);
+      const bValue = isNaN(parseFloat(b[key])) ? (ascending ? Number.POSITIVE_INFINITY : Number.NEGATIVE_INFINITY) : parseFloat(b[key]);
+  
+      if (ascending) {
+        return aValue - bValue;
+      } else {
+        return bValue - aValue;
+      }
+    };
+  
+    if (option === 'default') {
+      sortedStocks.sort((a, b) => new Date(b.updated) - new Date(a.updated));
+    } else if (option === 'alphabetical') {
+      sortedStocks.sort((a, b) => a.ticker.localeCompare(b.ticker));
+    } else if (option === 'cheapest') {
+      sortedStocks.sort((a, b) => sortFunction(a, b, 'buyP'));
+    } else if (option === 'expensive') {
+      sortedStocks.sort((a, b) => sortFunction(a, b, 'sellP', false));
+    }
+  
+    setStocks(sortedStocks);
+  };
+  
+
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const filteredStocks = stocks.filter(stock =>
+    stock.ticker.toLowerCase().includes(searchQuery.toLowerCase()) ||
+    (stock.type && stock.type.toLowerCase().includes(searchQuery.toLowerCase()))
+  );
+
+  return (
+    <div className="stock-container">
+      <div className='flex justify-end'>
+      <div className="search-bar">
+          <input 
+            type="text" 
+            placeholder="Search by name..." 
+            value={searchQuery} 
+            onChange={handleSearchChange}
+          />
+        </div>
+        <div className="sort-dropdown">
+          <label htmlFor="sort">Sort: </label>
+          <select 
+              id="sort" 
+              value={sortOption} 
+              onChange={handleSortChange} 
+              disabled={isDropdownDisabled}
+            >
+            <option value="default">Default</option>
+            <option value="alphabetical">Alphabetically</option>
+            <option value="cheapest">Cheapest</option>
+            <option value="expensive">Most Expensive</option>
+          </select>
+          <FontAwesomeIcon icon={faChevronDown} />
+        </div>
+      </div>
+
+      <div className={`stock-list ${selectedStock ? 'blurred' : ''}`}>
+        {filteredStocks.length === 0 ? <p>No stocks available</p> : null}
+        {filteredStocks.map(stock => (
+          <div key={stock.ticker} className="stock-item" onClick={() => handleStockClick(stock)}>
+            <img src={`/logos/${stock.ticker.toLowerCase()}.svg`} alt={`${stock.ticker} icon`} className="stock-icon" />
+            <div className="stock-ticker">{stock.ticker}</div>
+            <div className="stock-prices">
+              <div className="sell-price">{stock.sellP === "-" ? <>No sell<br />order</> : <>Bid:<br />${stock.sellP}</>}</div>
+              <div className="buy-price">{stock.buyP === "-" ? <>No buy<br />order</> : <>Ask:<br />${stock.buyP}</>}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+      {selectedStock && (
+        <StockPopup
+          //key={selectedStock.ticker} // Use key to force re-render
+          stock={selectedStock} 
+          onClose={closePopup} 
+          userId={userId} 
+          userBalance={userBalance} 
+          inventory={inventory} 
+          refreshUserData={refreshUserData}
+          //stocks={stocks} // Pass the full stocks list
+          //refreshStock={refreshStock} // Pass refreshStock callback
+        />
+      )}
+    </div>
+  );
+}
+
+export default StockList;
