7cf85672b0db6f4d37470e2ceee89d1538268071
Cepumins <cepumm@gmail.com>
2024-08-04 21:09:27 +0300

    0.3.2 - organizing

--- src/features/stocks/StockPopup.js
+++ src/features/stocks/StockPopup.js
@@ -16,7 +16,63 @@ function StockPopup({ stock, onClose, userId, userBalance, inventory, refreshUse
   const [orderDetails, setOrderDetails] = useState(stock); // Initialize with stock details
   const [confirmDetails, setConfirmDetails] = useState({}); // For storing details to confirm order
   const [isConfirmOpen, setIsConfirmOpen] = useState(false);
+  const [canBuy, setCanBuy] = useState(false);
+  const [hasStock, setHasStock] = useState(false);
 
+  useEffect(() => {
+    document.addEventListener('mousedown', handleOverlayClick);
+    document.addEventListener('keydown', handleEscKey); // Add keydown event listener
+    if (isCreateOrderOpen) {
+      //document.addEventListener('mousedown', handleCreateOrderOverlayClick);
+    }
+    if (isConfirmOpen) {
+      //document.addEventListener('mousedown', handleConfirmOrderOverlayClick);
+    }
+    return () => {
+      document.removeEventListener('mousedown', handleOverlayClick);
+      document.removeEventListener('keydown', handleEscKey); // Remove keydown event listener
+      //document.removeEventListener('mousedown', handleCreateOrderOverlayClick);
+      //document.removeEventListener('mousedown', handleConfirmOrderOverlayClick);
+    };
+  }, [isCreateOrderOpen, isConfirmOpen]);
+
+  const handleOverlayClick = (e) => {
+    if (!isCreateOrderOpen && !isConfirmOpen && popupRef.current && !popupRef.current.contains(e.target)) {
+      onClose();
+    }
+  };
+
+  const handleEscKey = (e) => {
+    if (e.key === 'Escape') {
+      onClose();
+    }
+  };
+
+  useEffect(() => {
+    setOrderDetails(stock); // Reset orderDetails when stock prop changes
+  }, [stock]);
+
+  useEffect(() => {
+    if (!isCreateOrderOpen) {
+      setOrderDetails(stock); // Reset orderDetails when CreateOrderPopup is closed
+    }
+  }, [isCreateOrderOpen, stock]);
+
+  useEffect(() => {
+    if (!isConfirmOpen) {
+      setOrderDetails(stock); // Reset orderDetails when ConfirmOrderPopup is closed
+    }
+  }, [isConfirmOpen, stock]);
+
+
+  useEffect(() => {
+    checkUserBalance();
+  }, [userBalance, stock.buyP]);
+
+  useEffect(() => {
+    checkUserInventory();
+  }, [stock]);
+  
   /*
   useEffect(() => {
     console.log(`POPUP: ${stock.ticker} new buy: ${stock.buyP} and sell: ${stock.sellP}`);
@@ -57,6 +113,27 @@ function StockPopup({ stock, onClose, userId, userBalance, inventory, refreshUse
   }, [stock]);
   */
 
+  const openCreateOrderPopup = (type) => {
+    //console.log('Opening create order');
+    setOrderType(type);
+    //const bestPrice = type === 'buy' ? stock.buyP : stock.sellP;
+    //setOrderDetails({ ticker: stock.ticker, icon: stock.icon, price: bestPrice });
+    setIsCreateOrderOpen(true);
+  };
+
+  const closeCreateOrderPopup = () => {
+    //console.log('Closing create order');
+    setIsCreateOrderOpen(false);
+    setOrderType('');
+    setOrderDetails(stock); // Refresh the order details from the stock
+  };
+
+  
+  const closeConfirmPopup = () => {
+    setIsConfirmOpen(false);
+    setConfirmDetails({});
+  };
+  
 
   const handleBuyAtPrice = async () => {
     if (userId && stock.buyP !== "-") {
@@ -106,17 +183,7 @@ function StockPopup({ stock, onClose, userId, userBalance, inventory, refreshUse
   }, [stock, userId, userBalance]);
   */
 
-  const handleOverlayClick = (e) => {
-    if (!isCreateOrderOpen && !isConfirmOpen && popupRef.current && !popupRef.current.contains(e.target)) {
-      onClose();
-    }
-  };
 
-  const handleEscKey = (e) => {
-    if (e.key === 'Escape') {
-      onClose();
-    }
-  };
 
   /*
   const handleCreateOrderOverlayClick = (e) => {
@@ -133,27 +200,7 @@ function StockPopup({ stock, onClose, userId, userBalance, inventory, refreshUse
   };
   */
 
-  const openCreateOrderPopup = (type) => {
-    console.log('Opening create order');
-    setOrderType(type);
-    //const bestPrice = type === 'buy' ? stock.buyP : stock.sellP;
-    //setOrderDetails({ ticker: stock.ticker, icon: stock.icon, price: bestPrice });
-    setIsCreateOrderOpen(true);
-  };
 
-  const closeCreateOrderPopup = () => {
-    console.log('Closing create order');
-    setIsCreateOrderOpen(false);
-    setOrderType('');
-    setOrderDetails(stock); // Refresh the order details from the stock
-  };
-
-  
-  const closeConfirmPopup = () => {
-    setIsConfirmOpen(false);
-    setConfirmDetails({});
-  };
-  
 
   /*
   const handleBuy = async () => {
@@ -171,38 +218,23 @@ function StockPopup({ stock, onClose, userId, userBalance, inventory, refreshUse
   };
   */
 
-  useEffect(() => {
-    document.addEventListener('mousedown', handleOverlayClick);
-    document.addEventListener('keydown', handleEscKey); // Add keydown event listener
-    if (isCreateOrderOpen) {
-      //document.addEventListener('mousedown', handleCreateOrderOverlayClick);
-    }
-    if (isConfirmOpen) {
-      //document.addEventListener('mousedown', handleConfirmOrderOverlayClick);
-    }
-    return () => {
-      document.removeEventListener('mousedown', handleOverlayClick);
-      document.removeEventListener('keydown', handleEscKey); // Remove keydown event listener
-      //document.removeEventListener('mousedown', handleCreateOrderOverlayClick);
-      //document.removeEventListener('mousedown', handleConfirmOrderOverlayClick);
-    };
-  }, [isCreateOrderOpen, isConfirmOpen]);
+  const checkUserBalance = () => {
+    console.log('checking user balance');
+    const balance = parseFloat(userBalance);
+    console.log(`user balance: ${balance}`);
+    const buyPrice = parseFloat(stock.buyP);
+    setCanBuy(balance >= buyPrice);
+  };
 
-  useEffect(() => {
-    setOrderDetails(stock); // Reset orderDetails when stock prop changes
-  }, [stock]);
+  const checkUserInventory = () => {
+    console.log('checking user inventory');
+    const hasStock = inventory.some(item => {
+      console.log(`stock ${item.ticker} in inventory: ${item.quantity}`);
+      return item.ticker === stock.ticker && item.quantity > 0;
+    });
+    setHasStock(hasStock);
+  };
 
-  useEffect(() => {
-    if (!isCreateOrderOpen) {
-      setOrderDetails(stock); // Reset orderDetails when CreateOrderPopup is closed
-    }
-  }, [isCreateOrderOpen, stock]);
-
-  useEffect(() => {
-    if (!isConfirmOpen) {
-      setOrderDetails(stock); // Reset orderDetails when ConfirmOrderPopup is closed
-    }
-  }, [isConfirmOpen, stock]);
 
   const hasStockInInventory = () => {
     console.log('checking users inventory again');
@@ -239,19 +271,19 @@ function StockPopup({ stock, onClose, userId, userBalance, inventory, refreshUse
               <button 
                 className="sell-button" 
                 onClick={handleSellAtPrice} 
-                disabled={!userId || !hasStockInInventory() || stock.sellP === "-"}
+                disabled={!userId || !hasStock || stock.sellP === "-"}
               >
                 {orderDetails.sellP === "-" ? "No Sell Offers" : `Sell at $${stock.sellP}`}
               </button>
               <button 
                 className="buy-button" 
                 onClick={handleBuyAtPrice} 
-                disabled={!userId || !canBuyStock() || stock.buyP === "-"}
+                disabled={!userId || !canBuy || stock.buyP === "-"}
               >
                 {orderDetails.buyP === "-" ? "No Buy Offers" : `Buy at $${stock.buyP}`}
               </button>
               <div></div>
-              <button className="create-sell-order-button" onClick={() => openCreateOrderPopup('sell')} disabled={!userId || !hasStockInInventory()}>Create Sell Order</button>
+              <button className="create-sell-order-button" onClick={() => openCreateOrderPopup('sell')} disabled={!userId || !hasStock}>Create Sell Order</button>
               <button className="create-buy-order-button" onClick={() => openCreateOrderPopup('buy')} disabled={!userId}>Create Buy Order</button>
             </div>
           </div>
